05afaf21dbd2f00e21ecee21f20e4c39802139b1
ZakariaALZ1 <zakaria.next.com@gmail.com>
2026-02-14 08:56:07 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.ENV = { 
-  SUPABASE_URL: "https://deovtpdjugfkccnpxfsm.supabase.co", 
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlb3Z0cGRqdWdma2NjbnB4ZnNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4ODk1MDMsImV4cCI6MjA4NjQ2NTUwM30.gEoj1WhdkyOcJQ3bf66FbTxneKmPvqspSHzu3Rd-W8A", 
+  SUPABASE_URL: "https://mxecvkhtleecopiqqloh.supabase.co", 
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14ZWN2a2h0bGVlY29waXFxbG9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNDA3MzksImV4cCI6MjA4NjYxNjczOX0.oYrRPY0uQWSwzaScjPRlXJja6M2a69TJ72-Whjb4rPM", 
   MAX_UPLOAD_SIZE: 104857600, // 100 MB
   ALLOWED_FILE_TYPES: ['.zip', '.rar', '.7z', '.baldimod'] 
 };
