259b65b925441ccbafb145214245f8fec91ca929
ZakariaALZ1 <zakaria.next.com@gmail.com>
2026-02-14 09:07:15 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.ENV = { 
   SUPABASE_URL: "https://mxecvkhtleecopiqqloh.supabase.co", 
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14ZWN2a2h0bGVlY29waXFxbG9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNDA3MzksImV4cCI6MjA4NjYxNjczOX0.oYrRPY0uQWSwzaScjPRlXJja6M2a69TJ72-Whjb4rPM", 
-  MAX_UPLOAD_SIZE: 104857600, // 100 MB
+  MAX_UPLOAD_SIZE: 104857600,
   ALLOWED_FILE_TYPES: ['.zip', '.rar', '.7z', '.baldimod'] 
 };
